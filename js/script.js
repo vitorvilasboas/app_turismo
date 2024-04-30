54d9de06e07d53ab4feafa0ee4b1cd3ea25968e9
NextageTop1 <vitorgabrielgb6@gmail.com>
2024-04-30 11:55:18 -0300

    nada

--- js/script.js
+++ js/script.js
@@ -1,43 +1,51 @@
-var caixa_menu = document.getElementsByTagName("aside")[0]
+// var caixa_menu = document.getElementsByTagName("aside")[0]
 
 
 
-function menu() {
+// function menu() {
  
-    caixa_menu.style.top = "0"
-    // caixa_menu.style.width = "100%"
-    caixa_menu.style.display = "inline-block"
+//     caixa_menu.style.top = "0"
+//     // caixa_menu.style.width = "100%"
+//     caixa_menu.style.display = "inline-block"
 
-}
+// }
 
-function voltar_menu() {
+// function voltar_menu() {
 
    
-    caixa_menu.style.top = "-410px";
-    // caixa_menu.style.width = "300px";;
+//     caixa_menu.style.top = "-410px";
+//     // caixa_menu.style.width = "300px";;
     
-}
-var TrandingSlider = new Swiper('.tranding-slider', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 2.5,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  });
+// }
+// var TrandingSlider = new Swiper('.tranding-slider', {
+//     effect: 'coverflow',
+//     grabCursor: true,
+//     centeredSlides: true,
+//     loop: true,
+//     slidesPerView: 'auto',
+//     coverflowEffect: {
+//       rotate: 0,
+//       stretch: 0,
+//       depth: 100,
+//       modifier: 2.5,
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     }
+//   });
 
-  var ul = document.querySelector('nav ul');
-  var menuBtn = document.querySelector('.menu-btn i');
+//   var ul = document.querySelector('nav ul');
+//   var menuBtn = document.querySelector('.menu-btn i');
+
+
+  const categoria = document.getElementsByClassName("categoria_img");
+
+
+  console.log(categoria[0])
+
+  categoria.forEach((element) => element.display = "none");
